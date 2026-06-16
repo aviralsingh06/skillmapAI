e6912537ff8c45e6aa6d4928334185500fef4aae
Aviral <aviralsingh66605@gmail.com>
2026-06-16 22:12:43 +0530

    Connected Neon DB schema

--- database/create_tables.py
+++ database/create_tables.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine, text
 from urllib.parse import quote_plus
 
-DB_USER = "postgres"
-DB_PASSWORD = quote_plus("Aviral66605@")
-DB_NAME = "skillmap_ai"
+# Neon database credentials
+DB_USER = "neondb_owner"
+DB_PASSWORD = quote_plus("npg_56CclADGzZdn")
+DB_HOST = "ep-dawn-bar-ahz05xs0.c-3.us-east-1.aws.neon.tech"
+DB_PORT = "5432"
+DB_NAME = "neondb"
 
 DATABASE_URL = (
     f"postgresql+psycopg://"
     f"{DB_USER}:{DB_PASSWORD}"
-    f"@127.0.0.1:5432/{DB_NAME}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?sslmode=require"
 )
 
 engine = create_engine(DATABASE_URL)
