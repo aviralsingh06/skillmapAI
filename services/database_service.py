dbbc0280ddb1fa295bdf50582ec498c736c9ee8e
Aviral <aviralsingh66605@gmail.com>
2026-06-16 20:19:33 +0530

    Fix Neon database connection

--- services/database_service.py
+++ services/database_service.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
@@ -5,16 +6,17 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = st.secrets.get("DB_USER", "")
+DB_HOST = st.secrets.get("DB_HOST", "")
+DB_PORT = st.secrets.get("DB_PORT", "5432")
+DB_NAME = st.secrets.get("DB_NAME", "")
+DB_PASSWORD = quote_plus(st.secrets.get("DB_PASSWORD", ""))
 
 DATABASE_URL = (
     f"postgresql+psycopg://"
     f"{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?sslmode=require"
 )
 
 engine = create_engine(DATABASE_URL)
